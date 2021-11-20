d96c6467a97993ae030917c239ce9866e3017768
Vaggelhs <konsvag@gmail.com>
2021-11-20 22:52:48 +0200

    home styles updated

--- screens/home/homeStyles.ts
+++ screens/home/homeStyles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const homeStyles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "grey",
+        backgroundColor: "#24282F",
         alignItems: "center",
         justifyContent: "center",
     },
