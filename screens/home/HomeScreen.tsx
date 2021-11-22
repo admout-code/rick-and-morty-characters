33690ae8b6d47271ed4c19e6d72166b743aeec7c
Vaggelhs <konsvag@gmail.com>
2021-11-22 12:40:09 +0200

    Home screen changed

--- screens/home/HomeScreen.tsx
+++ screens/home/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { View } from "react-native";
+import { Image, TouchableOpacity, View, Text } from "react-native";
 import { RootStackParamList } from "../../entities/navigation";
 import Button from "../../ui-kit/Button";
 import { homeStyles } from "./homeStyles";
@@ -11,7 +11,10 @@ function HomeScreen({ navigation }: Props) {
     const styles = homeStyles;
     return (
         <View style={styles.container}>
-            <Button onPress={() => navigation.navigate("CharactersList")}>Wubba lubba dub dub!</Button>
+            <TouchableOpacity onPress={() => navigation.navigate("CharactersList")}>
+                <Image style={styles.image} source={require("../../assets/rick_and_morty-min.png")} />
+                <Image style={styles.continueBtn} source={require("../../assets/continue_btn-min.png")} />
+            </TouchableOpacity>
         </View>
     );
 }
--- screens/home/homeStyles.ts
+++ screens/home/homeStyles.ts
@@ -7,4 +7,12 @@ export const homeStyles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
     },
+    image: {
+        width: 340,
+        height: 325,
+    },
+    continueBtn: {
+        width: 350,
+        height: 100,
+    }
 });
